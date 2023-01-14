f4c972e6b7f59ba696138eca6e0bb57faa3e772c
JSPlmOqnd <josapaoq@gmail.com>
2023-01-14 10:56:04 -0500

    Primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,67 @@
+const { response, request } = require('express');
+
+
+const usuariosGet = (req, res = response) => {
+
+    /* const query = req.query;
+
+    res.json({
+        msg: 'get API - controlador',
+        query
+    }); */
+
+    const { q, nombre = 'No name', apikey, page = 1, limit = 10 } = req.query;
+
+    res.json({
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+
+    const { nombre, edad } = req.body;
+
+    res.json({
+        msg: 'post API - usuariosPost',
+        nombre,
+        edad
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+
+    const { id } = req.params;
+
+    res.json({
+        msg: 'put API - usuariosPut',
+        id
+    });
+}
+
+const usuariosPatch = (req, res = response) => {
+
+    res.json({
+        msg: 'patch API - usuariosPatch'
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+
+    res.json({
+        msg: 'delete API - usuariosDelete'
+    });
+}
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosDelete,
+    usuariosPatch
+}
